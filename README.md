3d6d2d74209ba85905accd78efaf74402afdc1f3
Isaac <icruz@rappi.com>
2018-07-11 11:58:28 -0500

    Structure And Frameworks Added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # compose-bitso-statistics
+
+## Built With
+
+Here is a run-down of the core technologies used in this project.
+
+1. [API - ExpressJS](https://www.npmjs.com/package/node-typescript-koa-rest) - Boilerplate for API REST using NodeJS and KOA2, typescript. Logging and JWT as middlewares. TypeORM with class-validator, SQL CRUD.
+
+2. [BACKUP - Python](https://apscheduler.readthedocs.io/en/latest/) - Is a Python library that lets you schedule your Python code to be executed later, either just once or periodically.
+
+3. [DASHBOARD - REACT](https://reactjs.org/) - A very popular JavaScript DOM rendering framework for building scalable web applications using a component-based architecture.
+
+4. [DATABASE - POSTGRESQL](https://www.postgresql.org/) - The world's most advanced open source database
--- backup/scheduler.py
+++ backup/scheduler.py
@@ -1,10 +1,63 @@
-from apscheduler.schedulers import background
+import datetime, psycopg2,csv,json,requests,  os, sys, argparse
+from os.path import join, dirname
+from dotenv import load_dotenv
+from pytz import utc
 
-scheduler = background.BlockingScheduler()
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from apscheduler.executors.pool import ProcessPoolExecutor
+from apscheduler.executors.pool import ThreadPoolExecutor
 
-def minute_schedule():
-    for i in range(100):
-        print("scheduler run started.")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-scheduler.add_job(minute_schedule, 'interval', seconds=5)
+arser = argparse.ArgumentParser()
+parser.add_argument('-period','--periood', help='Period in minutes',required=True)
+args = parser.parse_args()
+
+DBNAME = os.getenv('DBNAME')
+DBUSER = os.getenv('DBUSER')
+DBPASSWORD = os.getenv('DBPASSWORD')
+DBHOST = os.getenv('DBHOST')
+DBPORT = os.getenv('DBPORT')
+
+def dictfetchall(cursor):
+    columns = [col[0] for col in cursor.description]
+    return [dict(zip(columns, row)) for row in cursor.fetchall()]
+
+def getBitoInfo():
+    return [] 
+
+def analyze(cursor):
+    for quota in getBitoInfo(cursor):
+        try:
+            new_state = get_quota_state(quota)
+            cursor.execute("""
+                UPDATE quotas.storekeeper_quotas SET quota_state_id = '{0}' where id = {1}
+            """.format(new_state, quota['id']))
+            print quota, new_state
+        except Exception as e:
+            print e.message
+    print "End looking on bitso ..."
+
+def init():
+    gyconnector = psycopg2.connect(database = DBNAME, user = DBUSER, password = DBPASSWORD, host = DBHOST, port = DBPORT)
+    gyconnector.autocommit = True
+    cursor = gyconnector.cursor()
+    analyze(cursor)
+    gyconnector.close()
+
+jobstores = {
+    'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite')
+}
+executors = {
+    'default': ThreadPoolExecutor(20),
+    'processpool': ProcessPoolExecutor(5)
+}
+job_defaults = {
+    'coalesce': False,
+    'max_instances': 10
+}
+scheduler = BlockingScheduler(executors=executors, job_defaults=job_defaults, timezone=utc)
+scheduler.add_job(init, 'cron', hour=hour)
 scheduler.start()
