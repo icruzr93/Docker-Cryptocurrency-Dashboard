f2c31757352d98f283fbdd8fe3cf33f43738fd81
Isaac <icruz@rappi.com>
2018-07-12 00:15:24 -0500

    Scheduler Near Completed

--- backup/scheduler.py
+++ backup/scheduler.py
@@ -11,20 +11,49 @@ from apscheduler.executors.pool import ThreadPoolExecutor
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
-DBNAME = os.getenv('DBNAME')
+BITSO_API = os.getenv('BITSO_API') + '/v3/ticker/'
+EXCHANGE_API = os.getenv('BACKUP_API') + '/exchange'
+AUTHORIZATION = os.getenv('AUTHORIZATION')
 MINUTES = 10
 
-def getBitcoinInfo():
-    return [] 
+books = [
+    "btc_mxn", "eth_mxn", "xrp_btc", "xrp_mxn", "eth_btc", "ltc_btc", "ltc_mxn", "bch_mxn", "tusd_btc", "tusd_mxn"
+]
+
+def getExchange(book):
+    response = requests.request(
+        "GET", BITSO_API, params={"book":book}
+    )
+    return json.loads(response.text)
+
+def postExchange(payload):
+    payloadToSave = {
+        'high': payload['high'],
+        'last': payload['last'],
+        'created_at': payload['created_at'],
+        'book': payload['book'],
+        'volume': payload['volume'],
+        'vwap': payload['vwap'],
+        'low': payload['low'],
+        'ask': payload['ask'],
+        'bid': payload['bid']
+    }
+    response = requests.request(
+        "POST", EXCHANGE_API, data=payloadToSave
+    )
+    return json.loads(response.text)
 
 def analyze():
-    for quota in getBitcoinInfo():
+    for book in books:
         try:
-            print(quota)
+            data = getExchange(book)
+            print(data)
+            insert = postExchange(data['payload'])
+            print(insert)
         except Exception as e:
-            print(e.message)
-    print("End looking on bitso ...")
+            print(e)
 
+analyze()
 print("Cron Started ...")
 
 jobstores = {
